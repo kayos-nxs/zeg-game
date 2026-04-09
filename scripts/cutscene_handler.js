8775754e5dfdf91120ae21a27a4ec676bebff6a5
KonstiantynK <kostiakor16@gmail.com>
2026-04-09 11:13:14 +0200

    Add Cutscene class for handling cutscenes
    Initial implementation of Cutscene class with methods for handling cutscenes.

---
+++ scripts/cutscene_handler.js
@@ -0,0 +1,11 @@
+// TODO:
+// *    add cutscene handler
+
+class Cutscene {
+  // take stuff from ASSETS folder
+
+  stopCutscene();
+  playCutscene(cutscene_name) {
+    // play current cutscene.. maybe add videos or make it in-game stuff
+  }
+}
